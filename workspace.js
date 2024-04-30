3c195a495a6a61056c11893a9df22cca0d750eaa
nicholas-stoll22 <nicholascourselstoll@gmail.com>
2024-04-30 18:19:12 -0500

    Connected html and .js, tested through simple innerHTML insertion

--- workspace.js
+++ workspace.js
@@ -1,12 +1,33 @@
-const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
 
 let humanScore = 0
 let computerScore = 0
 
-function playRound() {
+const rock = document.getElementById("rock");
+rock.addEventListener("click", humanRock);
 
+function humanRock() {
+  document.getElementById("text").innerHTML = "rock";
 }
 
-function playerSelection {
-    
+const paper = document.getElementById("paper");
+paper.addEventListener("click", humanPaper);
+
+function humanPaper() {
+  document.getElementById("text").innerHTML = "paper";
 }
+
+const scissors = document.getElementById("scissors");
+scissors.addEventListener("click", humanScissors);
+
+function humanScissors() {
+  document.getElementById("text").innerHTML = "scissors";
+}
+
+// function playRound() {
+
+// }
+
+// function playerSelection {
+
+// }
